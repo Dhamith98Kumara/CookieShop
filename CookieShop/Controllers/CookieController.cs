237f4161d6b7b590ee0cfd353e4402433792d941
Damithkumara <damithkumararoxx55@gmail.com>
2022-10-28 22:25:28 +0530

    complated MVC

---
+++ CookieShop/Controllers/CookieController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CookieShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CookieShop.Controllers
+{
+    public class CookieController : Controller
+    {
+        public static List<CookieViewModel> cookie = new List<CookieViewModel>();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Order()
+        {
+            var obj = new CookieViewModel();
+            return View(obj);
+        }
+
+        public IActionResult OrderCookie(CookieViewModel cvm)
+        {
+            cookie.Clear();
+            cookie.Add(cvm);
+            return RedirectToAction(nameof(ThankYou));
+        }
+
+        public IActionResult ThankYou()
+        {
+            return View(cookie);
+        }
+    }
+}
---
+++ CookieShop/Models/CookieViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CookieShop.Models
+{
+    public class CookieViewModel
+    {
+        public string Username { get; set; }
+        public int CookieCount { get; set; }
+        public string Place { get; set; }
+        public string Flavour { get; set; }
+        public string RedeemCode { get; set; }
+    }
+}
